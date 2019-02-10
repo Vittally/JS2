ba38f7d0d33154e7a75ebd440b9c61699561a010
vitaly <head@courierist.ru>
2019-02-10 11:20:37 +0300

    Create master

---
+++ lesson3.02/form.js
@@ -0,0 +1,12 @@
+class validate {
+    constructor(field, regexp){
+        this.field = field;
+        this.regexp = regexp;
+    }
+    check(field){
+        regexp.test(field){
+            alert('Все ок')
+        }
+    }
+
+}
